26ef510b192eb99f38d3821a90e3e61feff56a44
Radoslav Ivanov <rivanov2@protonmail.com>
2017-12-03 01:07:14 -0600

    :100: add done/not-done ability

--- todo-list-item.component.ts
+++ todo-list-item.component.ts
@@ -8,17 +8,25 @@ import TodoModel from './todo.model';
 @Component({
     selector: 'cmp-todo-item',
     template: `
-        <li>{{model.id}}:{{model.what}}:{{model.done}}</li>
+        <li>
+            {{model.id}}:{{model.what}}:{{model.done}}
+            <input #completed (click)="onChecked()" type="checkbox" />
+        </li>
     `
 })
 export default class TodoListItemComponent {
     private static idCounter = 1;
-
     @ViewChild(TemplateRef) public tmplOfThis: TemplateRef<TodoListItemComponent>;
     @Input() model: TodoModel;
     @Input() id = TodoListItemComponent.idCounter;
     @Input() what = '';
+    @ViewChild('checked') checked: HTMLInputElement;
     @Input() done = false;
 
+
     constructor() { TodoListItemComponent.idCounter++; }
+
+    onChecked() {
+        this.model.done = !this.model.done;
+    }
 }
