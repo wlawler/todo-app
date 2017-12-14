67055eb2571dd477f6c1e65888c530c2802f0e8d
wes lawler <wes.lawler2@gmail.com>
2017-12-13 21:03:19 -0600

    121317 comments part two

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+//This typescript file imports two more modules. On another note, I just realized that to comment in typescript is to use a different command than to comment in html//
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
