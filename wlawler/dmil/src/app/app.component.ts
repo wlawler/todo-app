af34e3728e8fc07e6fe9cdd9e3ca6d33b3155660
wes lawler <wes.lawler2@gmail.com>
2018-09-16 16:34:49 -0400

    made basic site

--- wlawler/dmil/src/app/app.component.ts
+++ wlawler/dmil/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dance moves Im Learning';
+  title = 'Dance Moves Im Learning';
 }
