82143fdb34fa10f76127e761fd4bb2cbf098d3a9
wes lawler <wes.lawler2@gmail.com>
2018-09-27 16:55:48 -0400

    practiced string interpolation with salsa component

--- wlawler/dmil/src/app/salsa/salsa.component.ts
+++ wlawler/dmil/src/app/salsa/salsa.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalsaComponent implements OnInit {
 
+  salsaStatus:string="Not Learned";
+
+  getSalsaStatus(){
+    return this.salsaStatus;
+  }
   constructor() { }
 
   ngOnInit() {
