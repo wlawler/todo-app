841b83187125ba57bcb694c144a404010aca9ae5
wes lawler <wes.lawler2@gmail.com>
2018-09-27 17:00:32 -0400

    practiced string interpolation with the zouk component

--- wlawler/dmil/src/app/zouk/zouk.component.ts
+++ wlawler/dmil/src/app/zouk/zouk.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZoukComponent implements OnInit {
 
+  zoukMoveStatus:string = 'Not Learned';
   constructor() { }
 
   ngOnInit() {
