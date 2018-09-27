b66f95c2e06f108682bd8081c15c898254109117
wes lawler <wes.lawler2@gmail.com>
2018-09-27 16:36:57 -0400

    practiced string interpolation in bachata component

--- wlawler/dmil/src/app/bachata/bachata.component.ts
+++ wlawler/dmil/src/app/bachata/bachata.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bachata.component.css']
 })
 export class BachataComponent implements OnInit {
-
+moveStatus: string = 'Not Learned';
   constructor() { }
 
   ngOnInit() {
